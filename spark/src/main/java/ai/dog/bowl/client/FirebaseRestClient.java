d139689f1e3b5fd954d68b19625ffd2b111d45ee
Hugo Freire <hugo@dog.ai>
2017-02-25 12:38:04 +0100

    feat(firebase): add retry logic

--- spark/src/main/java/ai/dog/bowl/client/FirebaseRestClient.java
+++ spark/src/main/java/ai/dog/bowl/client/FirebaseRestClient.java
@@ -6,19 +6,26 @@ package ai.dog.bowl.client;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+import net.jodah.failsafe.RetryPolicy;
 
 import java.util.Map;
 import java.util.SortedMap;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static net.jodah.failsafe.Failsafe.with;
 
 public class FirebaseRestClient {
   private static final String AUTH_PARAM_NAME = "auth";
   private static final String PATH_FORMAT = "%s.json";
 
   private final Client client;
+
+  private final RetryPolicy retryPolicy;
 
   private final String url;
   private final String token;
@@ -30,23 +37,28 @@ public class FirebaseRestClient {
     ClientConfig config = new DefaultClientConfig();
     config.getClasses().add(JacksonJsonProvider.class);
     this.client = Client.create(config);
+
+    this.retryPolicy = new RetryPolicy()
+            .retryIf((ClientResponse response) -> response.getStatus() >= 500)
+            .withBackoff(4, 32, SECONDS)
+            .withMaxRetries(5);
   }
 
   public void setValue(String path, Map value) {
-    client.resource(url)
+    with(retryPolicy).run(() -> client.resource(url)
             .path(String.format(PATH_FORMAT, path))
             .queryParam(AUTH_PARAM_NAME, token)
             .type(APPLICATION_JSON).entity(value)
-            .put(SortedMap.class);
+            .put(SortedMap.class));
   }
 
   public void updateValue(String path, Map value) {
-    client.resource(url)
+    with(retryPolicy).run(() -> client.resource(url)
             .path(String.format(PATH_FORMAT, path))
             .queryParam(AUTH_PARAM_NAME, token)
             .header("X-HTTP-Method-Override", "PATCH")
             .type(APPLICATION_JSON).entity(value)
-            .post(Map.class);
+            .post(Map.class));
   }
 
   public Map getValueAsMap(String path) {
@@ -69,9 +81,9 @@ public class FirebaseRestClient {
   }
 
   public void deleteValue(String path) {
-    client.resource(url)
+    with(retryPolicy).run(() -> client.resource(url)
             .path(String.format(PATH_FORMAT, path))
             .queryParam(AUTH_PARAM_NAME, token)
-            .delete(String.class);
+            .delete(String.class));
   }
 }
