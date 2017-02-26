0d76a453094c17d498e71b4fd2d90882968a6321
Hugo Freire <hugo@dog.ai>
2017-02-26 21:11:54 +0100

    test(spark): remove test for now

--- server/src/test/java/ai/dog/bowl/client/firebase/FirebaseClientTest.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2017, Hugo Freire <hugo@dog.ai>. All rights reserved.
- */
-
-package ai.dog.bowl.client.firebase;
-
-public class FirebaseClientTest {
-  private final FirebaseClient target = new FirebaseClient("my-url", "my-token");
-}
